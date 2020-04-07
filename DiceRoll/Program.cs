2297d27e83907919b06b4d422f3714e61b1b383b
JamesHarrys <bissickj@gmail.com>
2020-04-07 18:17:37 +0200

    Fourth project: Dice Roll

---
+++ DiceRoll/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DiceRoll {
+    
+    class Program {
+        
+        static void Main(string[] args) {
+
+            String answer;
+            do {
+                Console.WriteLine("🎲 Roll the dice:");
+                Random rand = new Random();
+                int randInt = rand.Next(1, 7);
+                Console.WriteLine("\n" + randInt);
+                Console.WriteLine("\n Roll the dice again? (Y/N)");
+                answer = Console.ReadLine();
+            } while (answer != "n");
+            
+        }
+        
+    }
+}
