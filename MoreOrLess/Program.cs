6f545df4b32ebd3562da1052cc21280b1542e0bd
James Bissick <jamesbissick@Macintosh.local>
2020-04-07 18:15:59 +0200

    Third project: More Or Less

---
+++ MoreOrLess/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MoreOrLess {
+    
+    class Program {
+        
+        static void Main(string[] args) {
+            // Welcome message
+            Console.WriteLine("Let's play \"Guess a Number!\"\n");
+            // CPU choose a random num between 1 & 100
+            Random rand = new Random();
+            int randInt = rand.Next(1, 101);
+            int count = 0;
+            int answer;
+            
+             do {
+                 // User tries to find the chosen num
+                 Console.WriteLine("Guess the number:");
+                 String inputUser = Console.ReadLine();
+                 
+                 if (int.TryParse(inputUser, out answer)) {
+                     if (answer > randInt) {
+                         Console.WriteLine("The number is lower.");
+                         count++;
+                     }
+                     else if (answer < randInt) {
+                         Console.WriteLine("The number is higher.");
+                         count++;
+                     }
+                     else if (answer == randInt) {
+                         Console.WriteLine("\n🎉 Congrats! You've found the secret number!");
+                         count++;
+                         // If user find the num, CPU display number of tries
+                         Console.WriteLine("Found in " + count + " tries.");
+                         Console.ReadLine();
+                     }
+                     else {
+                         Console.WriteLine("Something went wrong!");
+                     }
+                 }
+             } while (answer != randInt);
+        }
+        
+    }
+}
