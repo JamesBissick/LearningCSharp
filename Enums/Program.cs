7c58e1f660a76eab86944d42a3eeb55f9f0ba395
JamesHarrys <bissickj@gmail.com>
2020-04-07 18:19:17 +0200

    Course on Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Enums {
+    
+    class Program {
+        
+        // Declaring an enum
+        public enum Quality {
+            Gold,
+            Silver,
+            Bronze
+        }
+        
+        static void Main(string[] args) {
+
+            Quality product = Quality.Gold;
+            Console.WriteLine(product);
+            
+            Console.ReadLine();
+        }
+    }
+}
